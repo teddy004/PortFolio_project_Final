9ff9838a33ec4e94510eb157a575b7070043644c
teddy004 <tewodrosatirsaw4@gmail.com>
2023-05-19 11:07:11 -0300

    adding jsonwebtoken

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,14 @@
 const User = require("../models/User");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
 const router = require("express").Router();
 
 //UPDATE
-router.put("/:id", async (req, res) => {
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
       req.body.password,
@@ -26,7 +31,7 @@ router.put("/:id", async (req, res) => {
 });
 
 //DELETE
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json("User has been deleted...");
@@ -36,7 +41,7 @@ router.delete("/:id", async (req, res) => {
 });
 
 //GET USER
-router.get("/find/:id", async (req, res) => {
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const { password, ...others } = user._doc;
@@ -47,7 +52,7 @@ router.get("/find/:id", async (req, res) => {
 });
 
 //GET ALL USER
-router.get("/", async (req, res) => {
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
   const query = req.query.new;
   try {
     const users = query
@@ -61,7 +66,7 @@ router.get("/", async (req, res) => {
 
 //GET USER STATS
 
-router.get("/stats", async (req, res) => {
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
   const date = new Date();
   const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
 
